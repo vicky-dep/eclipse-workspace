261d56ba7bccd99dd3b6941b46e9e5e947b58297
vicky-dep <vicky@iesmijas.com>
2026-04-21 22:28:04 +0200

    Progress on Java exercises

--- T08_Ficheros/src/ejercicio09/PrincipalEj09.java
+++ T08_Ficheros/src/ejercicio09/PrincipalEj09.java
@@ -1,9 +1,13 @@
 package ejercicio09;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class PrincipalEj09 {
 
@@ -15,22 +19,56 @@ public class PrincipalEj09 {
 		
 		BufferedReader br1 = null;
 		BufferedReader br2 = null;
+		BufferedWriter bw = null;
+		
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
 		
 		try {
 			br1 = new BufferedReader(new FileReader("listaEnteros1.txt"));
 			br2 = new BufferedReader(new FileReader("listaEnteros2.txt"));
+			
+			String linea;
+			
+			linea = br1.readLine();
+			while (linea != null) {
+				numeros.add(Integer.parseInt(linea));
+				linea = br1.readLine();
+			}
+			
+			linea = br2.readLine();
+			while (linea != null) {
+				numeros.add(Integer.parseInt(linea));
+				linea = br2.readLine();
+			}
+			
+			Collections.sort(numeros);
+			
+			bw = new BufferedWriter(new FileWriter("resultado.txt"));
+			
+			for (int n : numeros) {
+				bw.write(String.valueOf(n));
+				bw.newLine();
+			}
+			
+			
 		} catch (FileNotFoundException e) {
 			System.out.println("Ficheros no encontrados");
 			e.printStackTrace();
-		}
-		
-		try {
-			String lineaTexto1 = br1.readLine();
-			String lineaTexto2 = br2.readLine();
 				
 		} catch (IOException e) {
 			System.out.println("Error al leer los ficheros");
 			e.printStackTrace();
+		} finally {
+			try {
+				br1.close();
+				br2.close();
+				bw.flush();
+				bw.close();
+			} catch (IOException e) {
+				System.out.println("Error al cerrar los buffers");
+				e.printStackTrace();
+			}
+			
 		}
 
 	}
